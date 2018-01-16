496d566be2ce89efe07d68caff9f8b24f3dfa549
Rishabh Khatri <rishabh15077@iiitd.ac.in>
2018-01-17 05:23:48 +0530

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get '/about', to: 'pages#about'
+
+  get '/contact', to: 'pages#contact'
+
+  get '/resume', to: 'pages#resume'
+
+  root 'pages#home'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
